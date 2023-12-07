12968be71efa5bf68ffcf643b8ca481145f273c6
Matthew Leveck <mleveck@gmail.com>
2023-12-07 02:38:39 -0800

    separate code out a bit

--- day5/5_mt.c
+++ day5/5_mt.c
@@ -1,10 +1,10 @@
 #include <limits.h>
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
 #include <sys/stat.h>
-#include <pthread.h>
 
 #define TABLE_CAP 100
 #define NTABLES 7
@@ -35,7 +35,7 @@ typedef struct work {
   int64_t start;
   int64_t stop;
   int64_t res;
-  table* tables;
+  table *tables;
 } work;
 
 void print_row(row r) { printf("%lld, %lld, %lld\n", r.dst, r.src, r.size); }
@@ -70,6 +70,7 @@ void parse_table(table *t) {
     tok = strtok(NULL, " :\n");
     r.src = strtoll(tok, NULL, 10);
     tok = strtok(NULL, " :\n");
+
     r.size = strtoll(tok, NULL, 10);
     *p = r;
     p++;
@@ -82,7 +83,27 @@ void parse_table(table *t) {
   }
 }
 
-void solver(void* w) {
+void parse_input(char *input, seeds *seeds, table *tables) {
+  parse_seeds(input, seeds);
+  puts("seeds:");
+  for (int i = 0; i < seeds->len; i++) {
+    printf("%lld ", seeds->seed[i]);
+  }
+  printf("\n");
+
+  for (int i = 0; i < NTABLES; i++) {
+    table *t = tables + i;
+    parse_table(t);
+  }
+
+  puts("tables:");
+  for (int i = 0; i < NTABLES; i++) {
+    printf("table %d:\n", i);
+    print_table(tables[i]);
+  }
+}
+
+void *solver(void *w) {
   work *work = w;
   int64_t res = LLONG_MAX;
   printf("starting work \n");
@@ -104,31 +125,24 @@ void solver(void* w) {
 
   printf("finishing work \n");
   work->res = res;
+  return NULL;
 }
 
-int64_t solve(const seeds seeds, const table *tables) {
-  int64_t res = LLONG_MAX;
-  for (int i = 0; i < seeds.len; i += 2) {
-    printf("Solving seed range %d\n", i);
-    int64_t start = seeds.seed[i];
-    int64_t end = start + seeds.seed[i + 1];
-    for (uint64_t seed = start; seed < end; seed++) {
-      int64_t mapped_val = seed;
-      for (int j = 0; j < NTABLES; j++) {
-        for (int64_t k = 0; k < tables[j].len; k++) {
-          row r = tables[j].rows[k];
-          if (mapped_val >= r.src && mapped_val < (r.src + r.size)) {
-            mapped_val = (mapped_val - r.src) + r.dst;
-            break;
-          }
-        }
-      }
-      if (mapped_val < res) {
-        res = mapped_val;
-      }
-    }
+void read_file(char *fname, char *ret_txt, size_t fsize, size_t strsize) {
+  FILE *inputf = fopen(fname, "rb");
+  if (!inputf) {
+    printf("Couldn't open file %s\n", fname);
+    exit(EXIT_FAILURE);
   }
-  return res;
+
+  int bytes_read = fread(ret_txt, 1, fsize, inputf);
+  if (bytes_read == -1) {
+    printf("error reading file %s", fname);
+    perror("Error");
+    exit(EXIT_FAILURE);
+  }
+  ret_txt[strsize - 1] = '\0';
+  fclose(inputf);
 }
 
 int main(int argc, char *argv[]) {
@@ -142,65 +156,41 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  char ftxt[input_st.st_size + 1];
+  size_t txtsize = input_st.st_size + 1;
 
-  FILE *inputf = fopen(fname, "rb");
-  if (!inputf) {
-    printf("Couldn't open file %s\n", fname);
-    exit(EXIT_FAILURE);
-  }
+  char input_txt[txtsize];
 
-  if (-1 == fread(ftxt, 1, input_st.st_size, inputf)) {
-    printf("error reading file %s", fname);
-    perror("Error");
-    exit(EXIT_FAILURE);
-  }
-  ftxt[input_st.st_size] = '\0';
-  char *fparse = ftxt;
+  read_file(fname, input_txt, input_st.st_size, txtsize);
+
   seeds seeds;
-  parse_seeds(fparse, &seeds);
-  puts("seeds:");
-  for (int i = 0; i < seeds.len; i++) {
-    printf("%lld ", seeds.seed[i]);
-  }
-  printf("\n");
-
   table tables[NTABLES];
-  for (int i = 0; i < NTABLES; i++) {
-    table *t = tables + i;
-    parse_table(t);
-  }
+  parse_input(input_txt, &seeds, tables);
 
-  puts("tables:");
-  for (int i = 0; i < NTABLES; i++) {
-    printf("table %d:\n", i);
-    print_table(tables[i]);
-  }
+  int num_ranges = seeds.len / 2;
 
-  work todo[seeds.len/2];
-  pthread_t ts[seeds.len/2];
-  for (int i= 0; i< seeds.len; i+=2){
+  work todo[num_ranges];
+  pthread_t ts[num_ranges];
+  for (int i = 0; i < seeds.len; i += 2) {
     work w;
     w.start = seeds.seed[i];
-    w.stop = seeds.seed[i+1] + w.start;
+    w.stop = seeds.seed[i + 1] + w.start;
     w.tables = tables;
-    todo[i/2] = w;
-    pthread_create(&ts[i/2], NULL, solver, (void *)&todo[i/2]);
+    todo[i / 2] = w;
+    pthread_create(&ts[i / 2], NULL, solver, (void *)&todo[i / 2]);
   }
 
-  for (int i=0; i < seeds.len/2; i ++){
+  for (int i = 0; i < seeds.len / 2; i++) {
     pthread_join(ts[i], NULL);
   }
 
   int64_t solution = todo[0].res;
-  for (int i =1; i < seeds.len/2; i++){
+  for (int i = 1; i < num_ranges; i++) {
     if (todo[i].res < solution) {
       solution = todo[i].res;
     }
   }
-  //int64_t solution = solve(seeds, tables);
+
   printf("solution: %lld\n", solution);
 
-  fclose(inputf);
   return EXIT_SUCCESS;
 }
